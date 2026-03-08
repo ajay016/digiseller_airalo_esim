2f6bdd98f55151ece3a83645c35bb208d48eabb2
ajay016 <your-email@example.com>
2026-03-08 22:19:55 +0600

    Fixed esimaccess email sending

--- digiseller/task.py
+++ digiseller/task.py
@@ -4,7 +4,7 @@ from digiseller.views import verify_unique_code_and_get_info
 
 MAX_RETRIES = 5
 
-@shared_task
+@shared_task(name="digiseller.task.retry_all_failed_orders")
 def retry_all_failed_orders():
     failed_qs = DigisellerFailedOrder.objects.all()
     for failed in failed_qs:
