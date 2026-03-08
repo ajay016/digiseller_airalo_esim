e57bf155a31f48763bd400eb7a223a05aa59916d
ajay016 <your-email@example.com>
2026-03-09 00:01:40 +0600

    Fixed esimaccess email sending

--- esim/admin.py
+++ esim/admin.py
@@ -377,7 +377,7 @@ class DigisellerOrderAdmin(admin.ModelAdmin):
             )
         }),
         ('Processing', {
-            'fields': ('status', 'digiseller_transaction_status', 'error_message', 'airalo_order')
+            'fields': ('status', 'digiseller_transaction_status', 'esim_email_sent', 'error_message', 'airalo_order')
         }),
         ('Tracking & Meta', {
             'fields': ('cart_uid', 'raw_payload', 'created_at', 'updated_at')
